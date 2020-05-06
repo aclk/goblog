41c11e0a94910bd8772c8a97b992ee18d50acef7
arvin.he <vk.he@qq.com>
2020-05-06 15:21:49 +0800

    update repo

---
+++ accountservice/go.mod
@@ -0,0 +1,5 @@
+module github.com/aclk/goblog/accountservice
+
+go 1.13
+
+require github.com/gorilla/mux v1.7.4
--- accountservice/main.go
+++ accountservice/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-        "fmt"
-        "github.com/callistaenterprise/goblog/accountservice/service"
+	"fmt"
+
+	"github.com/aclk/goblog/accountservice/service"
 )
 
 var appName = "accountservice"
 
 func main() {
-        fmt.Printf("Starting %v\n", appName)
-        service.StartWebServer("6767")
+	fmt.Printf("Starting %v\n", appName)
+	service.StartWebServer("6767")
 }
-
