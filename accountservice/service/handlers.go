45935b21e224596efa0a42bb2b84cd1ba842cc0a
arvin.he <vk.he@qq.com>
2020-05-08 05:58:13 +0800

    update repo for aclk

--- accountservice/service/handlers.go
+++ accountservice/service/handlers.go
@@ -1,13 +1,14 @@
 package service
 
 import (
-	"net/http"
-	"github.com/gorilla/mux"
 	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
 	"strconv"
-        "github.com/callistaenterprise/goblog/accountservice/dbclient"
-        "fmt"
-        "net"
+
+	"github.com/aclk/goblog/accountservice/dbclient"
+	"github.com/gorilla/mux"
 )
 
 var DBClient dbclient.IBoltClient
@@ -19,71 +20,70 @@ func GetAccount(w http.ResponseWriter, r *http.Request) {
 	// Read the 'accountId' path parameter from the mux map
 	var accountId = mux.Vars(r)["accountId"]
 
-        // Read the account struct BoltDB
+	// Read the account struct BoltDB
 	account, err := DBClient.QueryAccount(accountId)
-        account.ServedBy = getIP()
+	account.ServedBy = getIP()
 
-        // If err, return a 404
+	// If err, return a 404
 	if err != nil {
-                fmt.Println("Some error occured serving " + accountId + ": " + err.Error())
+		fmt.Println("Some error occured serving " + accountId + ": " + err.Error())
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-        // If found, marshal into JSON, write headers and content
+	// If found, marshal into JSON, write headers and content
 	data, _ := json.Marshal(account)
-        writeJsonResponse(w, http.StatusOK, data)
+	writeJsonResponse(w, http.StatusOK, data)
 }
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
-        // Since we're here, we already know that HTTP service is up. Let's just check the state of the boltdb connection
-        dbUp := DBClient.Check()
-        if dbUp && isHealthy {
-                data, _ := json.Marshal(healthCheckResponse{Status: "UP"})
-                writeJsonResponse(w, http.StatusOK, data)
-        } else {
-                data, _ := json.Marshal(healthCheckResponse{Status: "Database unaccessible"})
-                writeJsonResponse(w, http.StatusServiceUnavailable, data)
-        }
+	// Since we're here, we already know that HTTP service is up. Let's just check the state of the boltdb connection
+	dbUp := DBClient.Check()
+	if dbUp && isHealthy {
+		data, _ := json.Marshal(healthCheckResponse{Status: "UP"})
+		writeJsonResponse(w, http.StatusOK, data)
+	} else {
+		data, _ := json.Marshal(healthCheckResponse{Status: "Database unaccessible"})
+		writeJsonResponse(w, http.StatusServiceUnavailable, data)
+	}
 }
 
 func SetHealthyState(w http.ResponseWriter, r *http.Request) {
-        // Read the 'accountId' path parameter from the mux map
-        var state, err = strconv.ParseBool(mux.Vars(r)["state"])
-        if err != nil {
-                fmt.Println("Invalid request to SetHealthyState, allowed values are true or false")
-                w.WriteHeader(http.StatusBadRequest)
-                return
-        }
-        
-        isHealthy = state
-        w.WriteHeader(http.StatusOK)
+	// Read the 'accountId' path parameter from the mux map
+	var state, err = strconv.ParseBool(mux.Vars(r)["state"])
+	if err != nil {
+		fmt.Println("Invalid request to SetHealthyState, allowed values are true or false")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	isHealthy = state
+	w.WriteHeader(http.StatusOK)
 }
 
 func writeJsonResponse(w http.ResponseWriter, status int, data []byte) {
-        w.Header().Set("Content-Type", "application/json")
-        w.Header().Set("Content-Length", strconv.Itoa(len(data)))
-        w.WriteHeader(status)
-        w.Write(data)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
+	w.WriteHeader(status)
+	w.Write(data)
 }
 
 func getIP() string {
-        addrs, err := net.InterfaceAddrs()
-        if err != nil {
-                return "error"
-        }
-        for _, address := range addrs {
-                // check the address type and if it is not a loopback the display it
-                if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-                        if ipnet.IP.To4() != nil {
-                                return ipnet.IP.String()
-                        }
-                }
-        }
-        panic("Unable to determine local IP address (non loopback). Exiting.")
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "error"
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	panic("Unable to determine local IP address (non loopback). Exiting.")
 }
 
 type healthCheckResponse struct {
-        Status string `json:"status"`
+	Status string `json:"status"`
 }
-
